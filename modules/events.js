4ad9b7f83efdcf0a83100536425fe77b402ea6ac
NomanKhial <nomankhialb@gmail.com>
2025-09-06 01:57:54 +0500

    so we can call on event as many time as we want on our register event. just make sure the the listner goes first in order of excution of code before the event emssion

--- modules/events.js
+++ modules/events.js
@@ -10,3 +10,22 @@ event.on('MakeCustomEvent', ()=>console.log("Custom event has created!"))
 
 // * emit event to trigger the callback call
 event.emit('MakeCustomEvent')
+
+
+
+
+// *********************
+
+    //*** Emitting Multiple Events ***//
+
+//  ********************/
+
+event.on("order-pizza", (size, type)=>{
+    console.log(`Hey you've ordered ${type} pizza, of sized ${size}`)
+})
+
+event.on("order-pizza", (size, type)=>{
+    console.log(`Hey you've made ${type} pizza, of sized ${size}`)
+})
+event.emit('order-pizza', "large", "mushroom")
+event.emit('order-pizza', "small", "candy")
