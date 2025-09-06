ea3669993b8a093b22a6a76a9f65d5e446502bc5
NomanKhial <nomankhialb@gmail.com>
2025-09-07 00:30:24 +0500

    We learned how to make read and write stream and also pip them togthaer

---
+++ modules/streams.js
@@ -0,0 +1,11 @@
+
+const fs = require('fs')
+const path = require('path')
+
+const ReadStream = fs.createReadStream('../text.txt', 'utf-8')
+
+const WriteStream = fs.createWriteStream('../output.text', 'utf-8')
+
+ReadStream.pipe(WriteStream)
+
+WriteStream.on('finish', ()=> console.log('file has been successfully has been red'))
