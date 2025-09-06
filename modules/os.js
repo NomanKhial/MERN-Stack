c638a3551ab4d37b7e1dd31c009f9c874f3c82b2
NomanKhial <nomankhialb@gmail.com>
2025-09-07 00:14:50 +0500

    more on os module

--- modules/os.js
+++ modules/os.js
@@ -1,6 +1,15 @@
 
 const os = require('os')
 
+// console.log(os.version())
+
+
+console.log(os.arch()) //! returns the architecture of cpu
+const totalMemGB = (os.totalmem() / (1024 * 1024 * 1024)).toFixed(2);
+// console.log(totalMemGB)
+
+console.log(os.type())
+console.log(os.release())
 console.log(os.version())
-
-
+console.log(os.networkInterfaces())
+console.log(os.EOL)
